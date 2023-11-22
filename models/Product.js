ec6453c54b751a03ce9619f28ddf264c8ada2cca
Moudi <mjaderi97@gmail.com>
2023-11-23 10:10:23 +1100

    Update models: Added/modified model properties

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,19 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection.js');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+    },
   },
   {
     sequelize,
